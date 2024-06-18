10ce0934338d89a52bae0d9abbca2b377dcc69e3
SandipSoni <skk.soni@gmail.com>
2024-06-18 14:35:52 +0530

    Many features added

--- sandipsoni.py
+++ sandipsoni.py
@@ -12,3 +12,22 @@ print('Pull Req. 3')
 print('Pull Req. 4')
 print('Pull Req. 5')
 print('Pull Req. 6')
+
+print('<!----------------separator line --------------------------->')
+print('Python feature 1')
+print('Python feature 2')
+print('Python feature 3')
+print('Python feature 4')
+print('Python feature 6')
+print('Python feature 7')
+print('HTML feature 1')
+print('HTML feature 2')
+print('HTML feature 3')
+print('HTML feature 4')
+print('Pandas feature 1')
+print('Pandas feature 1')
+print('Numpy feature 1')
+print('Numpy feature 1')
+
+
+
