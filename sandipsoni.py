b8a4f290e7317fed1f8ac89249ebafad98f35f6b
SandipSoni <skk.soni@gmail.com>
2024-06-18 14:17:39 +0530

    pull requests print statement

--- sandipsoni.py
+++ sandipsoni.py
@@ -5,3 +5,10 @@ import numpy as np
 
 pull request --- # 1
 pull request --- # 2 new version of code
+
+print('Pull Req. 1')
+print('Pull Req. 2')
+print('Pull Req. 3')
+print('Pull Req. 4')
+print('Pull Req. 5')
+print('Pull Req. 6')
